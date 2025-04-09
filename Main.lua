b453bf84d3ab830a2dfe43fdf93a53f60e6d6ab3
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-09 20:56:41 -0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,11 @@
+local Library = require(game:GetService("ReplicatedStorage"):WaitForChild("Fluent"):WaitForChild("MainModule"))
 
+local Window = Library:Window{
+    Title = `Fluent {Library.Version}`,
+    ...
+}
+
+local Tabs = {
+    Main = Window:Tab{Title = "Main", Icon = "phosphor-users-bold"},
+    Settings = Window:Tab{Title = "Settings", Icon = "settings"}
+}
