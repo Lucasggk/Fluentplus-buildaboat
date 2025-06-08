aaf2e9c9b0e9613b6e73dc93b29abc72bbfe0925
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-08 14:41:09 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,9 +4,6 @@ end
 
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
 
-Button_Icon = "rbxassetid://137592432303285"
-              
-
 local Window = Fluent:CreateWindow({
     Title = "Build A Boat",
     SubTitle = "Feito por Lucas",
