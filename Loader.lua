78fc50cdcb61f512e8430cce43d24a3e06dacd7e
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-08 14:44:05 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -137,7 +137,7 @@ loja:AddButton({
     Title = "Compra o baú selecionado + quantidade",
     Callback = function()
         local vt = (tonumber(vpi) or 0) * (tonumber(ipv) or 0)
-        local md = 
+        local md = 1000
         if vt > md then
             local vtdc = vt - md
             Fluent:Notify({
