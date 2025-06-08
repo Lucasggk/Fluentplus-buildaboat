354746a029577757b16908ba851064c582935a16
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-08 15:50:05 -0300

    Update Teste.lua

--- Teste.lua
+++ Teste.lua
@@ -1,8 +1,13 @@
 getgenv().Script = [[
-loja:AddButton({
-    Title = "oi",
-    Callback = function()
-        print("oi")
+loja:AddInput("", {
+    Title = "Número de item para comprar\n",
+    Description = "Selecione o tanto de itens por compra.\n",
+    Default = "1",
+    Placeholder = "Número",
+    Numeric = true,
+    Finished = false,
+    Callback = function(Value)
+        ipv = Value 
     end
 })
 ]]
