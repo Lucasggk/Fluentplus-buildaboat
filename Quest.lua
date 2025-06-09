2ce84e43bbb0b67dde6074453fdcf33c071ea579
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-08 22:54:29 -0300

    Update Quest.lua

--- Quest.lua
+++ Quest.lua
@@ -3,19 +3,6 @@ function getquest(a)
     task.wait(1)
 end
 
-function tlob(selteam)
-    local plr = game.Players.LocalPlayer
-    local char = plr.Character or plr.CharacterAdded:Wait()
-    local hrp = char:WaitForChild("HumanoidRootPart", 5)
-
-    if not selteam or not hrp then return end
-
-    local part = selteam:FindFirstChild("Part1") or selteam:FindFirstChildWhichIsA("BasePart")
-    if part then
-        hrp.CFrame = part.CFrame + Vector3.new(0, 5, 0)
-    end
-end
-
 function Cloud(selteam)
     getquest(1)
     local plr = game.Players.LocalPlayer
