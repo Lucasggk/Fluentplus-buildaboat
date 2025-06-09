5ae64e57ab43155e299dd8d7c8679ecefcce4a73
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-09 02:03:54 -0300

    Update Quest Draco.lua

--- Quest Draco.lua
+++ Quest Draco.lua
@@ -1,1 +1,7 @@
-
+local spike = game:GetService("Players").LocalPlayer.Data.SpikeTrap.Value
+local spike_suf = 20
+local bspike
+local buyspike = 
+if spike < spike_suf then
+  bspike = spike_suf - spike
+  buyspike:FireServer(bspike, )
