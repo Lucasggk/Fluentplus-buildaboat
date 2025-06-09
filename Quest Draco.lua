39529d74afeab0f9f3b84e3309a1f8f48e20cfb1
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-09 02:16:31 -0300

    Update Quest Draco.lua

--- Quest Draco.lua
+++ Quest Draco.lua
@@ -2,7 +2,18 @@ local spike = game:GetService("Players").LocalPlayer.Data.SpikeTrap.Value
 local spike_suf = 20
 local bspike
 local buyspike = workspace.ItemBoughtFromShop 
+local din_nes
 if spike < spike_suf then
   bspike = spike_suf - spike
-  din = 
+  din = game.Players.LocalPlayer:WaitForChild("Data"):WaitForChild("Gold").Value
+  din_nes = bspike * 25
+  if din > din_nes then
   buyspike:InvokeServer("SpikeTrap", bspike)
+  else
+  print("sem dinheiro para a ação 1!")
+  return
+  end
+end 
+
+
+
