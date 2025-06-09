1efc1d09f62f70d61128dec80682ce32a7109078
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-08 22:23:41 -0300

    Create Quest.lua

---
+++ Quest.lua
@@ -0,0 +1,17 @@
+function getquest(a)
+    workspace.QuestMakerEvent:FireServer(a)
+    task.wait(1)
+end
+
+function Cloud()
+        getquest(1)
+        local plr = game.Players.LocalPlayer
+        local char = plr.Character or plr.CharacterAdded:Wait()
+        local hrp = char:WaitForChild("HumanoidRootPart", 5)
+        if not selteam then return end
+        local part = selteam.Quest.Cloud:FindFirstChild("Part1")
+        if hrp and part then
+            hrp.CFrame = part.CFrame
+        end
+end 
+
